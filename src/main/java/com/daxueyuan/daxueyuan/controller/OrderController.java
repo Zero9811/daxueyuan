782a4d6701caee10267e3d9d92704bc3b2c47427
sean <itseanzhong@gmail.com>
2019-03-02 19:55:41 +0800

    为order接口添加前置名"/order"

--- src/main/java/com/daxueyuan/daxueyuan/controller/OrderController.java
+++ src/main/java/com/daxueyuan/daxueyuan/controller/OrderController.java
@@ -12,10 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Date;
@@ -27,6 +24,7 @@ import java.util.List;
  */
 @RestController
 @Slf4j
+@RequestMapping("/order")
 public class OrderController {
 
     @Autowired
